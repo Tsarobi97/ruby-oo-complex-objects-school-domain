a64f6ed9652dc18d779b6c4fdd4c67cac958fa8a
RobertObi <robobi97@googlemail.com>
2021-02-28 18:26:37 -0600

    commit tree cc36b2435ba41df2d2adc869eb35db0cfc1890d0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+
+attr_accessor :name, :roster, :grade
+
+
+    def initialize(name)
+        @name = name
+        @roster = {} #built out to contain KEYS of grade levels + values (array of students)
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        self.roster.sort.each do |grade_array|
+            sorted_hash[grade_array[0]] = grade_array[1].sort
+        end
+        sorted_hash
+      end   
+    end
+
+
